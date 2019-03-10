335b5e0feda20c5e983395e6f89924a9a343239f
Douglas <douglascorreia17@gmail.com>
2019-03-10 15:17:40 -0300

    Alteração no CSS e no JS

--- js/main.js
+++ js/main.js
@@ -1,17 +1,50 @@
 $(document).ready(function() {
 	smoothScroll();
+	setNavigation();
 });
 
 function smoothScroll(){		
 	$('header .header-menu ul li a').on('click', function(element){
-
 		element.preventDefault();
-
 		var href = $(this).attr('href');
 
 		$('html, body').animate({
 			scrollTop: $(href).offset().top
 		}, 800);
+	});
+}
 
+function setNavigation(){
+	var sections = $('.section');
+	var header = $('header');
+
+	// Captura a altura do header
+	var headerHeight = header.outerHeight();
+
+	$(window).on('scroll', function(){
+		var currentPos = $(this).scrollTop();
+
+		sections.each(function(){
+			var top = $(this).offset().top - headerHeight;
+			var bottom = top + $(this).outerHeight();
+
+			if( currentPos >= top && currentPos <= bottom ){
+				header.find('a').removeClass('active');
+				header.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+			}
+		});
+	})
+
+	// Quando houver um recarregamento na página
+	var currentPosReload = $(window).scrollTop();
+
+	sections.each(function(){
+		var top = $(this).offset().top - headerHeight;
+		var bottom = top + $(this).outerHeight();
+
+		if( currentPosReload >= top && currentPosReload <= bottom ){
+			header.find('a').removeClass('active');
+			header.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+		}
 	});
 }
